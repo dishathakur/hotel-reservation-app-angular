c377559f8a2ffee9ba6ce8cc8776402814a9997d
disha-thakur-neudesic <disha.thakur@neudesic.com>
2024-12-05 18:46:02 +0530

    --created form structure for new reservation

---
+++ src/app/models/reservations.ts
@@ -0,0 +1,8 @@
+export interface Reservations {
+  id: string;
+  guestName: string;
+  guestEmail: string;
+  checkInDate: Date;
+  checkOutDate: Date;
+  roomNo: number;
+}
--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
-  styleUrls: ['./reservation-form.component.css']
+  styleUrls: ['./reservation-form.component.css'],
 })
 export class ReservationFormComponent {
+  reservationForm: FormGroup = new FormGroup({});
 
+  onSubmit = () => {};
 }
